211e7a3139f989a6827427bb2154cd0675e98407
Jeff Koftinoff <jeffk@jdkoftinoff.com>
2012-02-26 11:27:08 -0800

    project.mk added

---
+++ project.mk
@@ -0,0 +1,15 @@
+PROJECT=zeroconf-advertiser
+PROJECT_NAME=zeroconf-advertiser
+PROJECT_VERSION=20120226
+PROJECT_EMAIL=jeffk@jdkoftinoff.com
+PROJECT_LICENSE=ISC
+PROJECT_MAINTAINER=jeffk@jdkoftinoff.com
+PROJECT_COPYRIGHT=Copyright 2012
+PROJECT_DESCRIPTION=zeroconf-advertiser
+PROJECT_WEBSITE=http://github.com/jdkoftinoff/zeroconf-advertiser
+PROJECT_IDENTIFIER=com.statusbar.zeroconf-advertiser
+TOP_LIB_DIRS+=.
+CONFIG_TOOLS+=
+PKGCONFIG_PACKAGES+=
+
+
